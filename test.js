81bb185358805d7d7e1797a34641f86e521b9078
jiawen <159878273+j1awen@users.noreply.github.com>
2024-02-13 16:05:28 +0800

    added files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+function urmom() {
+    document.getElementById("pepe").innerHTML= "your mother HAHHAHA"
+}
+
+function register(){
+    const paraggraph = document.querySelector("p")
+    const username = 
+ document.getElementById("usernameInput").value;
+ paraggraph.innerHTML = username + ", you are signed up! yay!";
+}
+
